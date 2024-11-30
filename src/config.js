a4b82b998fa4005ca9dd90e85e9e24b753e42cc7
piemadd <piero@piemadd.com>
2024-11-30 14:54:02 -0600

    1.14.4 version

--- src/config.js
+++ src/config.js
@@ -596,7 +596,7 @@ export const config = {
   siteTitle: 'Transitstat.us',
   siteTitleOther: 'Transitstatus',
   tagLine: 'Open source, free, and easy transit tracker.',
-  version: 'v1.14.3',
+  version: 'v1.14.4',
   additionalWarnings: [],
   globalAlerts: [
     {
