8e0dc1d811868d883946778f1b83f1f45de07c2e
piemadd <piero@piemadd.com>
2024-12-08 01:39:10 -0600

    1.14.8

--- src/paths/index/changelog.jsx
+++ src/paths/index/changelog.jsx
@@ -33,6 +33,16 @@ const Changelog = () => {
           </tr>
         </thead>
         <tbody>
+          <tr id='1-14-8'>
+            <td>
+              <a href='#1-14-8'>v1.14.8</a>
+            </td>
+            <td>
+              <ul>
+                <li>Added MFF Shuttle.</li>
+              </ul>
+            </td>
+          </tr>
           <tr id='1-14-7'>
             <td>
               <a href='#1-14-7'>v1.14.7</a>
