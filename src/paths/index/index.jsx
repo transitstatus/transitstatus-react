2b0637830c234519a28db633df46854a50ca6c4d
piemadd <piero@piemadd.com>
2025-07-12 22:28:11 -0500

    oops

--- src/paths/index/index.jsx
+++ src/paths/index/index.jsx
@@ -34,7 +34,7 @@ const Index = () => {
     <main>
       <Oneko />
       <h1>{config.siteTitle}</h1>
-      <p>{config.siteTitleOther} {config.version} by <a href="https://piemadd.com" target="_blank">Piero</a></p>
+      <p>{config.siteTitleOther} {config.version} by <a href="https://piemadd.com" target="_blank">Piero&reg;</a></p>
       {config.additionalWarnings.map((warning, i) => {
         return <p key={i}>{warning}</p>;
       })}
