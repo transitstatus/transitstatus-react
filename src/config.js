cf1862eeaab74c54d4b2c693a254a8eaa7bcf378
piemadd <piero@piemadd.com>
2025-11-24 18:27:54 -0600

    oops forgot to bump the version number

--- src/config.js
+++ src/config.js
@@ -924,7 +924,7 @@ export const config = {
   siteTitle: 'Transitstat.us',
   siteTitleOther: 'Transitstatus',
   tagLine: 'Open source, free, and easy transit tracker.',
-  version: 'v1.20.6',
+  version: 'v1.20.8',
   additionalWarnings: [],
   globalAlerts: [
     {
