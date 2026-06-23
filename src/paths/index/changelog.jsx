1d2643bb8deca1b40261e308acad4cec36143fae
Piero Maddaleni <piero@piemadd.com>
2026-06-23 11:39:10 -0500

    1.21.2

--- src/paths/index/changelog.jsx
+++ src/paths/index/changelog.jsx
@@ -33,6 +33,16 @@ const Changelog = () => {
           </tr>
         </thead>
         <tbody>
+          <tr id="1-21-2">
+            <td>
+              <a href="#1-21-2">v1.21.2</a>
+            </td>
+            <td>
+              <ul>
+                <li>Updated PWA config.</li>
+              </ul>
+            </td>
+          </tr>
           <tr id="1-21-1">
             <td>
               <a href="#1-21-1">v1.21.1</a>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
-import appManifest from './manifest.json';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import appManifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    workbox: {
-      cleanupOutdatedCaches: false,
-      sourcemap: true,
-      navigateFallbackDenylist: [/store\.transitstat\.us/g]
-    },
-    manifest: appManifest
-  })],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: { navigateFallback: "index.html" },
+      workbox: { cleanupOutdatedCaches: false, sourcemap: true, navigateFallbackDenylist: [/store\.transitstat\.us/g] },
+      injectManifest: true,
+      manifest: appManifest
+    })
+  ]
+});
